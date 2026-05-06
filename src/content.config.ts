a78cd666cce4f6d8bd3bfb268c95ad18a1972199
chacha-kinako <atelier0maru@gmail.com>
2026-05-06 16:06:25 +0900

    fix: heroImageをstring型に変更（public/images/対応）

--- src/content.config.ts
+++ src/content.config.ts
@@ -13,7 +13,7 @@ const blog = defineCollection({
 			// Transform string to Date object
 			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
-			heroImage: z.optional(image()),
+			heroImage: z.string().optional(),
 			cardImage: z.string().optional(),
 			cardText: z.string().optional(),
 			featured: z.boolean().optional(),
