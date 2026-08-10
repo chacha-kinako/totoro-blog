e954893da4ec04daa2b3375d3daed4e7a00bb206
hinako <atelier0maru@gmail.com>
2026-08-10 23:24:44 +0900

    ブログ名・サブタイトルを「まんまる窓の小さな秘密基地」に変更
    図鑑カードのボタン文言変更も併せてコミット

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'エンジョイ！ジブリライフ！';
+export const SITE_TITLE = 'まんまる窓の小さな秘密基地';
 export const SITE_DESCRIPTION = 'ジブリグッズの本音レビュー＆暮らしのこと、のんびり発信中。';
